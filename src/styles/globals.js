0337ac90d3c012d2fabfcce0dadf9ec9e076ed8c
wfercanas <fernando.canas@outlook.com>
2023-03-10 15:17:39 -0500

    feat(globals): font-family and body global styles

---
+++ src/styles/globals.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: "Open Sans", sans-serif;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+export { GlobalStyle };
